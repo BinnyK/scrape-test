8361e1be53ac21374666f1b41ccc9f9069f001fd
Dabin Kim <binnywizard@gmail.com>
2018-07-06 18:03:41 +1000

    Add ids

--- fish.js
+++ fish.js
@@ -1,563 +1,563 @@
 [{
-    id: null,
+    id: 0,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0006/583449/albacore-th.fw.png",
     name: "Albacore",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 1,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0007/583450/amberjack-th.fw.png",
     name: "Amberjack",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 2,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0004/581944/atlantic-salmon-th.fw.png",
     name: "Atlantic salmon",
     water_type: "Freshwater",
     max_length: null
 }, {
-    id: null,
+    id: 3,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0008/583451/australian-bass-th.fw.png",
     name: "Australian Bass",
     water_type: "Freshwater and saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 4,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0011/583598/australian-salmon-th.fw.png",
     name: "Australian Salmon",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 5,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0008/583586/sawtail-th.fw.png",
     name: "Australian Sawtail",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 6,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0009/583605/balmain-bug-th.fw.png",
     name: "Balmain Bug",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 7,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0010/583453/bar-cod-th.fw.png",
     name: "Banded Rock Cod (Bar Cod)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 8,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0003/583455/bass-grouper-th.fw.png",
     name: "Bass Groper",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 9,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0006/581946/brook-trout-th.fw.png",
     name: "Brook trout",
     water_type: "Freshwater",
     max_length: null
 }, {
-    id: null,
+    id: 10,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0007/581947/brown-trout-th.fw.png",
     name: "Brown trout",
     water_type: "Freshwater",
     max_length: null
 }, {
-    id: null,
+    id: 11,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0010/583606/Beach-worm-th.fw.png",
     name: "Beach Worm",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 12,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0004/583456/big-eye-th.fw.png",
     name: "Bigeye Tuna",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 13,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0005/583457/black-bream-th.fw.png",
     name: "Black Bream",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 14,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0012/584985/abalone-th.fw.png",
     name: "Blacklip Abalone",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 15,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0003/583464/blue-eye-cod-th.fw.png",
     name: "Blue-Eye Trevalla (Cod)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 16,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0005/583484/groper-blue.fw.png",
     name: "Blue Groper",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 17,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0011/583463/blue-crab-th.fw.png",
     name: "Blue Crab",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 18,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0005/583466/bluefish-th.fw.png",
     name: "Bluefish",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 19,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0006/583467/bonito-th.fw.png",
     name: "Bonito",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 20,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0005/667319/common-carp-120.jpg",
     name: "Carp",
     water_type: "Freshwater",
     max_length: null
 }, {
-    id: null,
+    id: 21,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0011/583472/Cobia-th.fw.png",
     name: "Cobia",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 22,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0011/583607/cockle-th.fw.png",
     name: "Cockle",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 23,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0004/583609/Scallop-th.fw.png",
     name: "Commercial Scallop",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 24,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0012/583887/cunji-th.fw.png",
     name: "Cunjevoi",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 25,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0005/583583/red-rock-cod-Scorpionfish-th.fw.png",
     name: "Eastern Red Scorpionfish (Red Rock Cod)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 26,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0003/583509/lobster_t.fw.png",
     name: "Eastern Rock Lobster",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 27,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0011/583886/Eastern-School-Whiting-th.fw.png",
     name: "Eastern School Whiting",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 28,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0006/581955/eel-th.fw.png",
     name: "Eel",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 29,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0004/583474/estuary-perch-th.fw.png",
     name: "Estuary Perch",
     water_type: "Freshwater and saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 30,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0007/583477/f-head-sand-th.fw.png",
     name: "Flathead - Bluespotted",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 31,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0005/583475/f-head-dusky-th.fw.png",
     name: "Flathead Dusky",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 32,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0008/583478/f-head-tiger-th.fw.png",
     name: "Flathead Tiger",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 33,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0009/583479/flounder-th.fw.png",
     name: "Flounder",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 34,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0011/581960/freshwater-catfish-th.png",
     name: "Freshwater catfish",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 35,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0003/583482/garfish-th.fw.png",
     name: "Garfish",
     water_type: "Freshwater",
     max_length: null
 }, {
-    id: null,
+    id: 36,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0004/583483/gemfish-th.fw.png",
     name: "Gemfish",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 37,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0003/581961/golden-perch-th.fw.png",
     name: "Golden perch",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 38,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0004/583537/morwong-rubber-lip-th.fw.png",
     name: "Grey Morwong (Rubber Lip)",
     water_type: "Freshwater",
     max_length: null
 }, {
-    id: null,
+    id: 39,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0006/583485/hairtail-th.fw.png",
     name: "Hairtail",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 40,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0008/583505/harpuka-th.fw.png",
     name: "Hapuka",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 41,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0011/583508/leatherjacket-th.fw.png",
     name: "Leatherjacket",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 42,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0011/583562/tuna-Longtail-th.fw.png",
     name: "Longtail Tuna",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 43,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0007/583513/luderick-th.fw.png",
     name: "Luderick",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 44,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0004/583573/spanish-mackerel-th.fw.png",
     name: "Mackerel (Narrow-Barred Spanish)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 45,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0008/583514/mackerel-spotted-th.fw.png",
     name: "Mackerel (Spotted)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 46,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0003/583473/dolphinfish-th.fw.png",
     name: "Mahi Mahi (Dolphinfish)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 47,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0006/583521/man-jack-th.fw.png",
     name: "Mangrove Jack",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 48,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0007/583522/marlin-black-th.fw.png",
     name: "Marlin (Black)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 49,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0008/583523/marlin-blue-th.fw.png",
     name: "Marlin (Blue)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 50,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0009/583524/marlin-striped-th.fw.png",
     name: "Marlin (Striped)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 51,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0011/583535/morwong-banded-th.fw.png",
     name: "Morwong (Banded)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 52,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0009/583506/jackass-mowie-th.fw.png",
     name: "Morwong (Jackass)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 53,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0003/583536/morwong-red-th.fw.png",
     name: "Morwong (Red)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 54,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0010/583615/mosses-perch-th.fw.png",
     name: "Moses Snapper",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 55,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0011/583616/mud-crab_t.fw.png",
     name: "Mud Crab",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 56,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0005/583538/mullet-th.fw.png",
     name: "Mullet (Poddy)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 57,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0005/583538/mullet-th.fw.png",
     name: "Mullet (Sea)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 58,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0006/583539/mulloway-th.fw.png",
     name: "Mulloway",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 59,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0004/581962/murray-cod-t.fw.png",
     name: "Murray cod",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 60,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0005/581963/murray-cray-th.fw.png",
     name: "Murray crayfish",
     water_type: "Freshwater",
     max_length: null
 }, {
-    id: null,
+    id: 61,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0007/583540/octopus-th.fw.png",
     name: "Octopus",
     water_type: "Freshwater",
     max_length: null
 }, {
-    id: null,
+    id: 62,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0008/583541/pearl-perch-th.fw.png",
     name: "Pearl Perch",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 63,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0010/583885/Tiger-Prawn-th.fw.png",
     name: "Prawn (Black Tiger)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 64,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0009/583542/prawn_t.fw.png",
     name: "Prawn (School and Eastern King)",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 65,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0006/581964/rainbow-trout-th.fw.png",
     name: "Rainbow trout",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 66,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0006/667320/redfin-perch-120.jpg",
     name: "Redfin Perch",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 67,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0006/583611/rock-blackfish-th.fw.png",
     name: "Rock Blackfish",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 68,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0010/583597/sailfish-th.fw.png",
     name: "Sailfish",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 69,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0009/583596/samsonfish-th.fw.png",
     name: "Samonfish",
     water_type: "Freshwater",
     max_length: null
 }, {
-    id: null,
+    id: 70,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0009/583560/whiting-th.fw.png",
     name: "Sand Whiting",
     water_type: "Freshwater",
     max_length: null
 }, {
-    id: null,
+    id: 71,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0005/583520/mako-th.fw.png",
     name: "Shark - Mako",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 72,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0011/583580/school-shark-th.fw.png",
     name: "School Shark",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 73,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0007/583468/bronze-whaler-th.fw.png",
     name: "Shark - Whaler",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 74,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0008/583559/wobbegong-th.fw.png",
     name: "Shark - Wobbegong",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 75,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0010/583570/spearfish-th.fw.png",
     name: "Shortbill Spearfish",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 76,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0007/581965/silver-perch-th.fw.png",
     name: "Silver perch",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 77,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0006/584349/spiny-crayfish.png",
     name: "Spiny Crayfish",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 78,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0009/583578/snapper-juv-th.fw.png",
     name: "Snapper",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 79,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0007/583585/sbt-th.fw.png",
     name: "Southern Bluefin Tuna",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 80,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0003/583572/spanner_cb-t.fw.png",
     name: "Spanner Crab",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 81,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0009/583569/squid-th.fw.png",
     name: "Squid",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 82,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0008/583568/swordfish-th.fw.png",
     name: "Swordfish",
     water_type: "Freshwater",
     max_length: null
 }, {
-    id: null,
+    id: 83,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0009/583614/oysters_t.fw.png",
     name: "Oyster (Sydney Rock, Pacific, Native)",
     water_type: "Freshwater",
     max_length: null
 }, {
-    id: null,
+    id: 84,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0005/583565/tailor-th.fw.png",
     name: "Tailor",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 85,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0004/583564/tarwhine-th.fw.png",
     name: "Tarwhine",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 86,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0003/583563/teraglin-th.fw.png",
     name: "Teraglin",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 87,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0010/583579/silver-trevally-th.fw.png",
     name: "Trevallies",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 88,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0005/583610/turban-snail-th.fw.png",
     name: "Turban Snail",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 89,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0010/583561/wahoo-th.fw.png",
     name: "Wahoo",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 90,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0008/581966/yabby-th.fw.png",
     name: "Yabby",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 91,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0007/583558/yellowfin-bream-th.fw.png",
     name: "Yellowfin Bream",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 92,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0006/583557/yf-tuna-th.fw.png",
     name: "Yellowfin Tuna",
     water_type: "Saltwater",
     max_length: null
 }, {
-    id: null,
+    id: 93,
     img_th: "https://www.dpi.nsw.gov.au/__data/assets/image/0010/583507/kingfish-th.fw.png",
     name: "Yellowtail Kingfish",
     water_type: "Saltwater",
